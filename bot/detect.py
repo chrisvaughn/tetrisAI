fix rotation system

bot/detect.py
@@ -75,14 +75,10 @@ class Detectorist:
         if current_piece is None:
             return
         for piece in Tetrominoes:
-            for i in range(0, 4):
-                rp = np.rot90(current_piece, i)
-                if np.array_equal(rp, piece.detection_shape):
+            for i, s in enumerate(piece.detection_shapes):
+                if np.array_equal(current_piece, s):
                     self._current_piece = piece.clone()
-                    self._current_piece.set_position(
-                        at_x - piece.x_offset, at_y + piece.y_offset
-                    )
-                    self._current_piece.set_rotations(i)
+                    self._current_piece.set_detected_position(at_x, at_y, i)
                     return
 
     def _detect_next_piece(self):

tetris/pieces.py
@@ -1,173 +1,317 @@
 import copy
-from typing import Union
+from typing import Union, List, Tuple
 
 import numpy as np
+from .board import Board
 
 
 class Piece:
-    def __init__(
-        self,
-        name: str,
-        shape: np.ndarray,
-        valid_rots: int,
-        x_offset: int,
-        y_offset: int,
-        detection_shape: Union[np.ndarray, None] = None,
-    ):
+    def __init__(self, name: str, shapes: List[np.ndarray], default_shape_idx: int):
         self.name = name
-        self.shape = shape
-        self.valid_rotations = valid_rots
-        self.detection_shape = detection_shape
-        self.x = 0
-        self.y = 0
-        self.x_offset = x_offset
-        self.y_offset = y_offset
-        self.rot = 0
+        self.shapes = shapes
+        self.default_shape_idx = default_shape_idx
+        self._x = 0  # the center of the shape matrix
+        self._y = 0  # the center of the shape matrix
+        self.current_shape_idx = default_shape_idx
+        self._detection_shapes = None
 
     def __str__(self) -> str:
-        return f"Piece<name: {self.name}, x: {self.x}, y: {self.y}>"
+        return f"Piece<name: {self.name}, x: {self._x}, y: {self._y}>"
 
-    def set_position(self, x, y):
-        self.x = x
-        self.y = y
+    @property
+    def shape(self) -> np.ndarray:
+        return self.shapes[self.current_shape_idx]
 
-    def set_rotations(self, rot):
-        self.rot = rot
+    @property
+    def corner_xy(self) -> Tuple[int, int]:
+        return self._x - 2, self._y - 2
+
+    @property
+    def zero_based_corner_xy(self) -> Tuple[int, int]:
+        return self._x - 2 - 1, self._y - 2 - 1
+
+    # 0-based, left most x, top most y, number of rotations of detection shape
+    def set_detected_position(self, x: int, y: int, shape_idx: int):
+        self.current_shape_idx = shape_idx
+        where = np.where(self.shape == 1)
+        self._x = x + 2 - np.amin(where[1]) + 1
+        self._y = y + 2 - np.amin(where[0]) + 1
+
+    # 1-based, center of piece
+    def set_position(self, x: int, y: int):
+        if x < 1 or x > Board.columns:
+            raise ValueError(f"x must be between 1 and {Board.columns}")
+        if y < 1 or y > Board.rows:
+            raise ValueError(f"y must be between 1 and {Board.rows}")
+        self._x = x
+        self._y = y
 
     def move_down(self):
-        self.y += 1
+        self._y += 1
 
     def move_left(self):
-        self.x -= 1
+        self._x -= 1
 
     def move_right(self):
-        self.x += 1
+        self._x += 1
 
     def rotate_ccw(self):
-        self.shape = np.rot90(self.shape)
+        self.current_shape_idx = (self.current_shape_idx - 1) % len(self.shapes)
+
+    def rotate_cw(self):
+        self.current_shape_idx = (self.current_shape_idx + 1) % len(self.shapes)
 
     def clone(self):
         return copy.deepcopy(self)
+
+    @property
+    def detection_shapes(self) -> List[np.ndarray]:
+        if self._detection_shapes:
+            return self._detection_shapes
+
+        ds = []
+        for s in self.shapes:
+            where = np.where(s == 1)
+            pruned = s[
+                np.amin(where[0]) : np.amax(where[0]) + 1,
+                np.amin(where[1]) : np.amax(where[1]) + 1,
+            ]
+            ds.append(pruned)
+        self._detection_shapes = ds
+        return self._detection_shapes
 
 
 Tetrominoes = [
     Piece(
         "i",
-        np.array(
-            [
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [1, 1, 1, 1, 0],
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-            ],
-            dtype=int,
-        ),
+        [
+            np.array(
+                [
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 0, 0, 0],
+                ],
+                dtype=int,
+            ),
+            np.array(
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [1, 1, 1, 1, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                ],
+                dtype=int,
+            ),
+        ],
         1,
-        0,
-        2,
-        np.array([[1, 1, 1, 1]], dtype=int),
     ),
     Piece(
         "l",
-        np.array(
-            [
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 2, 2, 2, 0],
-                [0, 2, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-            ],
-            dtype=int,
-        ),
-        3,
+        [
+            np.array(
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 1, 1, 0],
+                    [0, 0, 0, 0, 0],
+                ],
+                dtype=int,
+            ),
+            np.array(
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 1, 1, 1, 0],
+                    [0, 1, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                ],
+                dtype=int,
+            ),
+            np.array(
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 1, 1, 0, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 0, 0, 0],
+                ],
+                dtype=int,
+            ),
+            np.array(
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 1, 0],
+                    [0, 1, 1, 1, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                ],
+                dtype=int,
+            ),
+        ],
         1,
-        2,
-        np.array([[1, 1, 1], [1, 0, 0]], dtype=int),
     ),
     Piece(
         "j",
-        np.array(
-            [
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 3, 3, 3, 0],
-                [0, 0, 0, 3, 0],
-                [0, 0, 0, 0, 0],
-            ],
-            dtype=int,
-        ),
+        [
+            np.array(
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 1, 1, 0, 0],
+                    [0, 0, 0, 0, 0],
+                ],
+                dtype=int,
+            ),
+            np.array(
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 1, 0, 0, 0],
+                    [0, 1, 1, 1, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                ],
+                dtype=int,
+            ),
+            np.array(
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 1, 1, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 0, 0, 0],
+                ],
+                dtype=int,
+            ),
+            np.array(
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 1, 1, 1, 0],
+                    [0, 0, 0, 1, 0],
+                    [0, 0, 0, 0, 0],
+                ],
+                dtype=int,
+            ),
+        ],
         3,
-        1,
-        2,
-        np.array([[1, 1, 1], [0, 0, 1]], dtype=int),
     ),
     Piece(
         "o",
-        np.array(
-            [
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 4, 4, 0, 0],
-                [0, 4, 4, 0, 0],
-                [0, 0, 0, 0, 0],
-            ],
-            dtype=int,
-        ),
+        [
+            np.array(
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 1, 1, 0, 0],
+                    [0, 1, 1, 0, 0],
+                    [0, 0, 0, 0, 0],
+                ],
+                dtype=int,
+            )
+        ],
         0,
-        1,
-        2,
-        np.array([[1, 1], [1, 1]], dtype=int),
     ),
     Piece(
         "s",
-        np.array(
-            [
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 0, 5, 5, 0],
-                [0, 5, 5, 0, 0],
-                [0, 0, 0, 0, 0],
-            ],
-            dtype=int,
-        ),
-        3,
-        1,
-        2,
-        np.array([[0, 1, 1], [1, 1, 0]], dtype=int),
+        [
+            np.array(
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 1, 1, 0],
+                    [0, 1, 1, 0, 0],
+                    [0, 0, 0, 0, 0],
+                ],
+                dtype=int,
+            ),
+            np.array(
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 1, 1, 0],
+                    [0, 0, 0, 1, 0],
+                    [0, 0, 0, 0, 0],
+                ],
+                dtype=int,
+            ),
+        ],
+        0,
     ),
     Piece(
         "t",
-        np.array(
-            [
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 6, 6, 6, 0],
-                [0, 0, 6, 0, 0],
-                [0, 0, 0, 0, 0],
-            ],
-            dtype=int,
-        ),
-        3,
-        1,
+        [
+            np.array(
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 1, 1, 1, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                ],
+                dtype=int,
+            ),
+            np.array(
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 1, 1, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 0, 0, 0],
+                ],
+                dtype=int,
+            ),
+            np.array(
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 1, 1, 1, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 0, 0, 0],
+                ],
+                dtype=int,
+            ),
+            np.array(
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 1, 1, 0, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 0, 0, 0],
+                ],
+                dtype=int,
+            ),
+        ],
         2,
-        np.array([[1, 1, 1], [0, 1, 0]], dtype=int),
     ),
     Piece(
         "z",
-        np.array(
-            [
-                [0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0],
-                [0, 7, 7, 0, 0],
-                [0, 0, 7, 7, 0],
-                [0, 0, 0, 0, 0],
-            ],
-            dtype=int,
-        ),
-        3,
-        1,
-        2,
-        np.array([[1, 1, 0], [0, 1, 1]], dtype=int),
+        [
+            np.array(
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 1, 1, 0, 0],
+                    [0, 0, 1, 1, 0],
+                    [0, 0, 0, 0, 0],
+                ],
+                dtype=int,
+            ),
+            np.array(
+                [
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 1, 0],
+                    [0, 0, 1, 1, 0],
+                    [0, 0, 1, 0, 0],
+                    [0, 0, 0, 0, 0],
+                ],
+                dtype=int,
+            ),
+        ],
+        0,
     ),
 ]

tetris/state.py
@@ -9,7 +9,12 @@ from .pieces import Piece
 
 
 class GameState:
-    def __init__(self, board: Board, current_piece: Piece, next_piece: Piece):
+    def __init__(
+        self,
+        board: Board,
+        current_piece: Union[Piece, None],
+        next_piece: Union[Piece, None],
+    ):
         self.board = board
         self.current_piece = current_piece
         self.next_piece = next_piece
@@ -29,22 +34,37 @@ class GameState:
                     (255, 255, 255),
                     cv2.FILLED if cell != 0 else None,
                 )
-        cp_shape = np.rot90(self.current_piece.shape, self.current_piece.rot)
-        for (y, x), value in np.ndenumerate(cp_shape):
-            if value != 0:
-                cv2.rectangle(
-                    virtual_board,
-                    (
-                        (self.current_piece.x + x) * block_size,
-                        (self.current_piece.y + y) * block_size,
-                    ),
-                    (
-                        (self.current_piece.x + x + 1) * block_size,
-                        (self.current_piece.y + y + 1) * block_size,
-                    ),
-                    (255, 0, 0),
-                    cv2.FILLED,
-                )
+        if self.current_piece:
+            px, py = self.current_piece.zero_based_corner_xy
+            for (y, x), value in np.ndenumerate(self.current_piece.shape):
+                if value != 0:
+                    cv2.rectangle(
+                        virtual_board,
+                        (
+                            (px + x) * block_size,
+                            (py + y) * block_size,
+                        ),
+                        (
+                            (px + x + 1) * block_size,
+                            (py + y + 1) * block_size,
+                        ),
+                        (255, 0, 0),
+                        cv2.FILLED,
+                    )
+                else:
+                    cv2.rectangle(
+                        virtual_board,
+                        (
+                            (px + x) * block_size,
+                            (py + y) * block_size,
+                        ),
+                        (
+                            (px + x + 1) * block_size,
+                            (py + y + 1) * block_size,
+                        ),
+                        (0, 255, 0),
+                        cv2.FILLED if x == 0 and y == 0 else None,
+                    )
 
         cv2.imshow("Virtual Board", virtual_board)
 
@@ -68,14 +88,18 @@ class GameState:
         return copy.deepcopy(self)
 
     def new_piece(self) -> bool:
+        if self.current_piece:
+            _, cp_y = self.current_piece.zero_based_corner_xy
+        else:
+            cp_y = 0
+        if self._last_piece:
+            _, lp_y = self._last_piece.zero_based_corner_xy
+        else:
+            lp_y = 0
         return (
             self.current_piece
             and self._last_piece is None
-            or (
-                self.current_piece
-                and self._last_piece
-                and self.current_piece.y < self._last_piece.y
-            )
+            or (self.current_piece and self._last_piece and cp_y < lp_y)
         )
 
     def move_down(self):
@@ -90,73 +114,98 @@ class GameState:
     def move_left(self):
         if self.move_left_possible():
             self.current_piece.move_left()
+            return True
+        return False
 
-    def move_right(self):
+    def move_right(self) -> bool:
         if self.move_right_possible():
             self.current_piece.move_right()
+            return True
+        return False
 
-    def move_down_possible(self):
+    def move_down_possible(self) -> bool:
+        px, py = self.current_piece.zero_based_corner_xy
+        py = py + 1
         for (y, x), value in np.ndenumerate(self.current_piece.shape):
-            tx = self.current_piece.x
-            ty = self.current_piece.y + 1
-            if value != 0 and ty + y >= 0:
-                if ty + y >= self.board.rows or self.board.board[ty + y, tx + x] != 0:
-                    return False
-        return True
-
-    def move_left_possible(self):
-        for (y, x), value in np.ndenumerate(self.current_piece.shape):
-            tx = self.current_piece.x - 1
-            ty = self.current_piece.y
-            if value != 0 and ty + y >= 0:
-                if tx + x < 0 or self.board.board[ty + y, tx + x] != 0:
-                    return False
-        return True
-
-    def move_right_possible(self):
-        for (y, x), value in np.ndenumerate(self.current_piece.shape):
-            tx = self.current_piece.x + 1
-            ty = self.current_piece.y
-            if value != 0 and tx + x >= 0 and ty + y >= 0:
+            if value != 0 and py + y >= 0:
                 if (
-                    tx + x >= self.board.columns
-                    or self.board.board[ty + y, tx + x] != 0
+                    py + y >= self.board.rows
+                    or px + x >= self.board.columns
+                    or self.board.board[py + y, px + x] != 0
+                ):
+                    return False
+        return True
+
+    def move_left_possible(self) -> bool:
+        px, py = self.current_piece.zero_based_corner_xy
+        px = px - 1
+        for (y, x), value in np.ndenumerate(self.current_piece.shape):
+            if value != 0 and py + y >= 0:
+                if px + x < 0 or self.board.board[py + y, px + x] != 0:
+                    return False
+        return True
+
+    def move_right_possible(self) -> bool:
+        px, py = self.current_piece.zero_based_corner_xy
+        px = px + 1
+        for (y, x), value in np.ndenumerate(self.current_piece.shape):
+            if value != 0 and px + x >= 0 and py + y >= 0:
+                if (
+                    px + x >= self.board.columns
+                    or self.board.board[py + y, px + x] != 0
                 ):
                     return False
         return True
 
     def rot_ccw_possible(self):
-        return self.rot_possible(np.rot90(self.current_piece.shape))
+        p = self.current_piece.clone()
+        p.rotate_ccw()
+        return self.rot_possible(p.shape)
 
-    def rot_possible(self, shape):
+    def rot_cw_possible(self):
+        p = self.current_piece.clone()
+        p.rotate_cw()
+        return self.rot_possible(p.shape)
+
+    def rot_possible(self, shape) -> bool:
+        px, py = self.current_piece.zero_based_corner_xy
         for (y, x), value in np.ndenumerate(shape):
-            tx = self.current_piece.x
-            ty = self.current_piece.y
-            if value != 0 and ty + y >= 0:
+            if value != 0 and py + y >= 0:
                 if (
-                    ty + y >= self.board.rows
-                    or tx + x < 0
-                    or tx + x >= self.board.columns
-                    or self.board.board[ty + y, tx + x] != 0
+                    py + y >= self.board.rows
+                    or px + x < 0
+                    or px + x >= self.board.columns
+                    or self.board.board[py + y, px + x] != 0
                 ):
                     return False
         return True
 
-    def rotate_ccw(self):
+    def rotate_ccw(self) -> bool:
         if self.rot_ccw_possible():
             self.current_piece.rotate_ccw()
+            return True
+        return False
+
+    def rotate_cw(self):
+        if self.rot_cw_possible():
+            self.current_piece.rotate_cw()
+            return True
+        return False
 
     def place_piece_on_board(self):
+        px, py = self.current_piece.zero_based_corner_xy
         for (y, x), value in np.ndenumerate(self.current_piece.shape):
             if value != 0:
                 self.board.board[
-                    self.current_piece.y + y, self.current_piece.x + x
+                    py + y,
+                    px + x,
                 ] = value
         self.board.updated()
 
     def check_game_over(self):
+        _, py = self.current_piece.zero_based_corner_xy
         for (y, x), value in np.ndenumerate(self.current_piece.shape):
-            if value != 0 and self.current_piece.y + y < 0:
+            if value != 0 and py + y < 0:
                 return True
         return False
 